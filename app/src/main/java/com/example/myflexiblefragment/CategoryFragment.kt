013168baa2b015d71bd835db1a296370b2621302
wdinx <wahyuudin2811@gmail.com>
2023-08-16 14:32:07 +0800

    Exercise Fragment

---
+++ app/src/main/java/com/example/myflexiblefragment/CategoryFragment.kt
@@ -0,0 +1,50 @@
+package com.example.myflexiblefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class CategoryFragment : Fragment(), View.OnClickListener {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        if(p0?.id == R.id.btn_detail_category){
+            val detailCategoryFragment = DetailCategoryFragment()
+
+            val bundle = Bundle()
+            bundle.putString(DetailCategoryFragment.EXTRA_NAME, "LifeStyle")
+            val description = "Ini adalah halaman yang akan menampilkan description"
+
+            detailCategoryFragment.arguments = bundle
+            detailCategoryFragment.description = description
+
+            val fragmentManager = parentFragmentManager
+
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+
+        }
+    }
+}
